4fe5bae09c8401658ada2d1539a9055ed73b1d15
Thenafi <65831153+Thenafi@users.noreply.github.com>
2022-07-06 02:03:11 +0600

    1st

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-
-# Flask
 from flask import Flask, render_template, jsonify, request, flash, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_login import login_required, current_user, login_user, logout_user
@@ -8,14 +5,19 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from werkzeug import secure_filename
 from flask_sqlalchemy import SQLAlchemy
+
 # OS
 import os
+
 # Config
 import config
+
 # Sessions
 from uuid import uuid4
+
 # Time
 import time
+
 # Forms
 from forms import LoginForm, RegisterForm
 
@@ -34,7 +36,9 @@ login_manager.init_app(app)
 db = SQLAlchemy(app)
 
 # Static path
-static_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "static"))
+static_path = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
+)
 
 
 @login_manager.user_loader
@@ -44,82 +48,83 @@ def load_user(user_id):
 
 # http://flask.pocoo.org/snippets/62/
 from urllib.parse import urlparse, urljoin
+
+
 def is_safe_url(target):
     ref_url = urlparse(request.host_url)
     test_url = urlparse(urljoin(request.host_url, target))
-    return test_url.scheme in ('http', 'https') and \
-           ref_url.netloc == test_url.netloc
+    return test_url.scheme in ("http", "https") and ref_url.netloc == test_url.netloc
 
 
 # LANDING
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
     error = None
     form = RegisterForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         if form.validate_on_submit():
-            print('Getting user data')
+            print("Getting user data")
             user = User(
-                name=request.form['username'],
-                email=request.form['email'],
-                password=request.form['password'],
-                forename=request.form['forename'],
-                surname=request.form['surname'],
+                name=request.form["username"],
+                email=request.form["email"],
+                password=request.form["password"],
+                forename=request.form["forename"],
+                surname=request.form["surname"],
             )
-            print('Adding user')
+            print("Adding user")
             db.session.add(user)
-            print('Commit')
+            print("Commit")
             db.session.commit()
-            print('Login')
+            print("Login")
             login_user(user)
-            return redirect(url_for('.dashboard'))
+            return redirect(url_for(".dashboard"))
         else:
-            error = 'Invalid data'
-    return render_template('register.html', form=form, error=error)
+            error = "Invalid data"
+    return render_template("register.html", form=form, error=error)
 
 
 # LOGIN
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     error = None
     form = LoginForm(request.form)
-    if request.method == 'POST':
+    if request.method == "POST":
         if form.validate_on_submit():
-            user = User.query.filter_by(name=request.form['username']).first()
+            user = User.query.filter_by(name=request.form["username"]).first()
             if user is not None and bcrypt.check_password_hash(
-                    user.password, request.form['password']
-                    ):
+                user.password, request.form["password"]
+            ):
                 login_user(user)
-                flash('You were logged in.')
+                flash("You were logged in.")
                 # https://flask-login.readthedocs.io/en/latest/#how-it-works
-                next = request.args.get('next')
+                next = request.args.get("next")
                 if not is_safe_url(next):
                     return abort(400)
-                return redirect(next or url_for('.dashboard'))
+                return redirect(next or url_for(".dashboard"))
             else:
-                error = 'Invalid username or password'
-    return render_template('login.html', form=form, error=error)
+                error = "Invalid username or password"
+    return render_template("login.html", form=form, error=error)
 
 
 # LOGOUT
-@app.route('/logout')
+@app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    flash('You were logged out.')
-    return redirect(url_for('.index'))
+    flash("You were logged out.")
+    return redirect(url_for(".index"))
 
 
 # DASHBOARD
-@app.route('/dashboard')
+@app.route("/dashboard")
 @login_required
 def dashboard():
-    return render_template('dashboard.html', name=current_user.name)
+    return render_template("dashboard.html", name=current_user.name)
 
 
 class User(db.Model):
@@ -154,9 +159,9 @@ class User(db.Model):
         return str(self.id)
 
     def __repr__(self):
-        return '<name - {}>'.format(self.name)
+        return "<name - {}>".format(self.name)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # RUN
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
